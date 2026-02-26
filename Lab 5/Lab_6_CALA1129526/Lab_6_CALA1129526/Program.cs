83136c9cad9188691ae8c7532195e5ac0e7b912c
DR1s-1 <christiancool06@gmail.com>
2026-02-25 19:46:05 -0600

    a

---
+++ Lab 5/Lab_6_CALA1129526/Lab_6_CALA1129526/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6_CALA1129526
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de usuario
+            Console.Write("¿Cómo te llamas? ");
+            string nombre = Console.ReadLine();
+            // Salida de datos
+            Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+
+            // Ejercicio #1
+            string bici = "bicicleta";
+            string moto = "motocicleta";
+            string carro = "carro";
+            string camion = "camión";
+            string bus = "bus";
+            string vehiculo;
+
+            Console.WriteLine("Ingrese el vehiculo");
+            vehiculo = Console.ReadLine();
+
+
+            if (vehiculo.ToLower() == bici)
+            {
+                Console.WriteLine("No motorizado");
+            }
+            else if (vehiculo.ToLower() == moto)
+            {
+                Console.WriteLine(" Ligero");
+            }
+            else if (vehiculo.ToLower()== carro)
+            {
+                Console.WriteLine("Mediano ");
+            }
+            else if (vehiculo.ToLower() == camion)
+            {
+                Console.WriteLine(" Pesado ");
+            }
+            else if (vehiculo.ToLower() == bus)
+            {
+                Console.WriteLine("Transporte público");
+            }
+            else
+            {                
+                Console.WriteLine("Vehículo no reconocido"); 
+            }
+
+
+            // Ejercicio #2 Banco Industrial
+
+            string TT1;
+
+
+
+
+
+
+
+        }
+    }
+}
