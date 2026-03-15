39e3ad3517ce7c8f2bdf92d7bb21114f19465e09
DR1s-1 <christiancool06@gmail.com>
2026-03-15 17:35:43 -0600

    aaaaa

---
+++ Lab8_CALA_1129526/Lab8_CALA_1129526/Lab8_CALA_1129526/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_CALA_1129526
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de usuario
+            Console.Write("¿Cómo te llamas? ");
+            string nombre = Console.ReadLine();
+            // Salida de datos
+            Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+
+            // Ejercicio 1
+            int note, cantidadapro = 0, CantRepro = 0, suma = 0;
+            double promedio = 0;
+            Console.WriteLine("Ejercicio#1");
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese las notas");
+                note = int.Parse(Console.ReadLine());
+                if (note <= 65)
+                {
+                    cantidadapro++;
+                }
+                else
+                {
+                    CantRepro++;
+                }
+                suma = suma + note;
+            }
+            promedio = suma / 10;
+            Console.WriteLine();
+            Console.WriteLine("Su promedio fue de: " + promedio);
+            Console.WriteLine();
+            Console.WriteLine("Los aprobados fueron: " + cantidadapro);
+            Console.WriteLine();
+            Console.WriteLine("Los reprobados fueron: " + CantRepro);
+            Console.WriteLine();
+
+
+            // Ejercicio 2
+
+
+
+
+        }
+    }
+}
---
+++ Lab8_CALA_1129526/Lab8_CALA_1129526/Lab8_CALA_1129526/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Lab8_CALA_1129526")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab8_CALA_1129526")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e64d998d-2196-4d9f-be5f-2266c043076b")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
