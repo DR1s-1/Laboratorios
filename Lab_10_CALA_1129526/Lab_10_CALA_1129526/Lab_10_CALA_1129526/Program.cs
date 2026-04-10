6a43e76876641e44a73e98e8c82271b3059a3247
DR1s-1 <christiancool06@gmail.com>
2026-04-09 19:27:05 -0600

    lab 10

---
+++ Lab_10_CALA_1129526/Lab_10_CALA_1129526/Lab_10_CALA_1129526/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10_CALA_1129526
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de usuario
+            Console.Write("¿Cómo te llamas? ");
+            string nombre = Console.ReadLine();
+            // Salida de datos
+            Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+        }
+    }
+}
